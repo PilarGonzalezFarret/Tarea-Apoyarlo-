a230861748192bd0a0c5c96d35af1f8ba2f4b1f9
PilarGonzalezFarret <37448643+PilarGonzalezFarret@users.noreply.github.com>
2023-01-06 00:01:16 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import './App.css';
+
+import PersonCard from './components/PersonCards',
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName='Jane' lastName='Doe' age={54} hairColor='Black' />
+      <PersonCard firstName='John' lastName='Smith' age={88} hairColor='Brown' />
+      <PersonCard firstName='Millard' lastName='Fillmore' age={50} hairColor='Brown' />
+      <PersonCard firstName='Maria' lastName='Smith' age={54} hairColor='Black' />
+    </div>
+  );
+}
+
+export default App;
---
+++ PersonCards.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class PersonalCard extends Component {
+    render() {
+        return(
+            <div>
+                <h1> { this.props.lastName }, { this.props.firstName} <h1/>
+                <p>Age: {this.props.age} <p/>
+                <p>Hair color: {this.props.hairColor} <p/>
+            <div/>
+        );
+    }
+}
+
+
+export default PersonalCard;
